e9e64a050aa20ca4865db74d741c37f62f2e92fa
KradNosnatef <fuqianshan@foxmail.com>
2021-06-25 20:53:11 +0800

    淦，为什么不能播放，给个理由

--- VideoWebsite.VueSpace/src/main.js
+++ VideoWebsite.VueSpace/src/main.js
@@ -6,6 +6,11 @@ import {
 	VueJsonp
 } from 'vue-jsonp'
 
+import VideoPlayer from 'vue-video-player'
+import 'vue-video-player/src/custom-theme.css'
+import 'video.js/dist/video-js.css'
+Vue.use(VideoPlayer)
+
 Vue.use(VueJsonp)
 
 Vue.config.productionTip = false
