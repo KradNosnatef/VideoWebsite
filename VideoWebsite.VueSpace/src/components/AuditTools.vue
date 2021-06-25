84f2f5ef4cada18137d161cfc5ab100ba9c69bfb
KradNosnatef <fuqianshan@foxmail.com>
2021-06-25 18:38:34 +0800

    前端下行测试

--- VideoWebsite.VueSpace/src/components/AuditTools.vue
+++ VideoWebsite.VueSpace/src/components/AuditTools.vue
@@ -1,15 +1,19 @@
 <template>
-	<div>???
+	<div>
 		<p>审核工具页面</p>
 		<p>{{inputHorizontalProp}}</p>
+		<a href="/Xapi/media" download="sb">wow</a>
 	</div>
 </template>
 
 <script>
+	import jsonp from "jsonp"
 	export default{
 		props:[
 			"inputHorizontalProp"
-		]
+		],
+		methods:{
+		}
 	}
 </script>
 
